2dac3f8adf3ad77a5c29e36335cb0d3d83f3637a
devusol <sean@devusol.com>
2022-11-07 20:06:36 -0500

    signal indication

--- index.js
+++ index.js
@@ -226,7 +226,6 @@ app.post("/", (req, res) => {
     });
 
     console.log("it's live data");
-    console.log(req.body.hotspots);
     const sysTime = bufferObj.readUInt32LE();
     const a = bufferObj.readUInt16BE(4);
     const aa = bufferObj.readUInt8(6);
